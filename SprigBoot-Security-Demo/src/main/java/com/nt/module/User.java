80616759803457dba7cd16a120d811e995226777
Rakesh Sur <rakeshsur72@gmail.com>
2023-08-21 11:07:22 +0530

    intial commit

---
+++ SprigBoot-Security-Demo/src/main/java/com/nt/module/User.java
@@ -0,0 +1,15 @@
+package com.nt.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+	
+	private String username;
+	private String password;
+	private String email;
+	
+
+}
